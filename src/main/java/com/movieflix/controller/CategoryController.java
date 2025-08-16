04a351e94284d2fc15f1f2d5766951df9883f473
carlos.henrique <carloshenriquesw@gmail.com>
2025-08-16 00:24:05 -0300

    feat: CRUD created successful

--- src/main/java/com/movieflix/controller/CategoryController.java
+++ src/main/java/com/movieflix/controller/CategoryController.java
@@ -1,14 +1,22 @@
 package com.movieflix.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.movieflix.model.Category;
 import com.movieflix.service.CategoryService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/movieflix/categories")
@@ -23,9 +31,35 @@ public class CategoryController {
 
 
 
-    @GetMapping("/AllCategories")
+    @GetMapping()
     public ResponseEntity<List<Category>> getAllCategories(){
         List<Category> categories = categoryService.findAllCategories();
         return ResponseEntity.ok(categories);
+    }
+
+    @PostMapping()
+    public ResponseEntity<Category> createCategory(@RequestBody Category category) {
+        categoryService.createdCategory(category);
+        return ResponseEntity.ok(category);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Category>> getIdCategory(@PathVariable Long id){
+        Optional<Category> category = categoryService.findIdCategory(id);
+        return ResponseEntity.ok(category);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletedCategory(@PathVariable Long id) {
+        categoryService.deletedCategory(id);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Category> updatedCategory(@PathVariable Long id, @Valid @RequestBody Category category){
+        Optional<Category> updated = categoryService.updateCategory(id, category);
+        if (updated.isPresent()){
+            return ResponseEntity.ok(updated.get());
+        }
+        return ResponseEntity.notFound().build();
     }
 }
--- src/main/java/com/movieflix/service/CategoryService.java
+++ src/main/java/com/movieflix/service/CategoryService.java
@@ -1,7 +1,7 @@
 package com.movieflix.service;
 
 import java.util.List;
-
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -18,10 +18,32 @@ public class CategoryService {
         this.categoryRepository = category;
     }
 
-
-
     public List<Category> findAllCategories() {
         return categoryRepository.findAll();
     }
 
+    public Category createdCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public Optional<Category> findIdCategory(Long id) {
+        return categoryRepository.findById(id);
+    }
+
+    public void deletedCategory(Long id) {
+        categoryRepository.deleteById(id);
+    }
+
+    public Optional<Category> updateCategory(Long id, Category updatedCategory) {
+        if (id == null || updatedCategory == null){
+            return Optional.empty();
+        }
+
+        return categoryRepository.findById(id)
+        .map(existing -> {
+            if (updatedCategory.getName() != null) {
+                existing.setName(updatedCategory.getName());
+            }return categoryRepository.save(existing);
+        });
+    }
 }
