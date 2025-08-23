80fd890ec61f2b2c62124b34fabc374b11cda7a0
carlos.henrique <carloshenriquesw@gmail.com>
2025-08-23 09:37:32 -0300

    feat: completed the implementation of users in the application

--- src/main/java/com/movieflix/controller/AuthController.java
+++ src/main/java/com/movieflix/controller/AuthController.java
@@ -1,16 +1,16 @@
 package com.movieflix.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.movieflix.DTOrequest.UserRequest;
 import com.movieflix.DTOresponse.UserResponse;
-import com.movieflix.mapper.CategoryMapper;
-import com.movieflix.model.Category;
+import com.movieflix.mapper.UserMapper;
 import com.movieflix.model.User;
-import com.movieflix.repository.UserRepository;
 import com.movieflix.service.UserService;
 
 import lombok.RequiredArgsConstructor;
@@ -18,14 +18,15 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/auth")
+@RequestMapping("/movieflix/auth")
 public class AuthController {
 
     private final UserService userService;
 
-
+    @PostMapping("/register")
     public ResponseEntity<UserResponse> register (@RequestBody UserRequest request){
-        User registerUser = userService.createdCategory(CategoryMapper.toUser(request));
+        User registerUser = userService.save(UserMapper.toUser(request));
+        return ResponseEntity.status(HttpStatus.CREATED).body(UserMapper.toUserResponse(registerUser));
     }
     
 
--- src/main/java/com/movieflix/mapper/UserMapper.java
+++ src/main/java/com/movieflix/mapper/UserMapper.java
@@ -1,5 +1,26 @@
 package com.movieflix.mapper;
 
-public class UserMapper {
 
+import com.movieflix.DTOrequest.UserRequest;
+import com.movieflix.DTOresponse.UserResponse;
+import com.movieflix.model.User;
+
+public class UserMapper {
+    public static User toUser(UserRequest request){
+        return User
+        .builder()
+        .name(request.name())
+        .email(request.email())
+        .password(request.password())
+        .build();
+    }
+
+    public static UserResponse toUserResponse(User user){
+        return UserResponse
+        .builder()
+        .id(user.getId())
+        .name(user.getName())
+        .email(user.getEmail())
+        .build();
+    }
 }
